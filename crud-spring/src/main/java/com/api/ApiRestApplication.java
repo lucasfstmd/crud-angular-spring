24d862c7e5fecbb6a106c05add9c4190e1e14d45
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-10-28 14:11:24 -0300

    API Rest Spring

--- crud-spring/src/main/java/com/api/ApiRestApplication.java
+++ crud-spring/src/main/java/com/api/ApiRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudSpringApplication {
+public class ApiRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudSpringApplication.class, args);
+		SpringApplication.run(ApiRestApplication.class, args);
 	}
 
 }
