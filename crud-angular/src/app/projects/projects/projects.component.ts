6dbdd060cbedaf6ecc2c47284a7d498de023ac8f
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-10-28 14:39:41 -0300

    Http request

--- crud-angular/src/app/projects/projects/projects.component.ts
+++ crud-angular/src/app/projects/projects/projects.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ProjectsModel } from '../models/projects.model';
 import { ProjectsService } from '../services/projects.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-
-  projects: ProjectsModel[] = [];
+  projects: Observable<ProjectsModel[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private ProjectsService: ProjectsService) { }
+  constructor(private ProjectsService: ProjectsService) {
     /* TODO document why this constructor is empty */
+    this.projects = this.ProjectsService.list();
+  }
 
   ngOnInit(): void {
     // TODO document why this method 'ngOnInit' is empty
-    this.projects = this.ProjectsService.list();
   }
 }
--- crud-angular/src/app/projects/services/projects.service.ts
+++ crud-angular/src/app/projects/services/projects.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ProjectsModel } from '../models/projects.model';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
+  //Caminho da API
+  private readonly API = '/crud-angular/src/assets/projects.json';
+
   //Injeção de dependencia pra acesso a API
   constructor(private httpClient: HttpClient) { }
 
-  list(): ProjectsModel[] {
-    return [
-      { _id: "1", name: "Angular", category: "Front-End"}
-    ];
+  list() {
+    return this.httpClient.get<ProjectsModel[]>(this.API).pipe(
+      tap(projects => console.log(projects))
+    );
   }
 }
  /* Classe de service independente, que passa as dependencias para os componentes*/
