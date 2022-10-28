012d6f82e84f9b798cfe2062dc4cc7c0efbebe8d
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-10-27 21:40:07 -0300

    added a card to the table and the route class

--- crud-angular/src/app/projects/projects.module.ts
+++ crud-angular/src/app/projects/projects.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { ProjectsComponent } from './projects/projects.component';
     CommonModule,
     ProjectsRoutingModule,
     MatTableModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class ProjectsModule { }
