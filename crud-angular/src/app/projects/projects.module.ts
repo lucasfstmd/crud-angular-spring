93a6dd6129ac5661044107e871fece505b5768ad
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-10-27 22:39:31 -0300

    class for routers

--- crud-angular/src/app/projects/projects.module.ts
+++ crud-angular/src/app/projects/projects.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 
 @NgModule({
@@ -15,9 +14,7 @@ import { ProjectsComponent } from './projects/projects.component';
   imports: [
     CommonModule,
     ProjectsRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule
+    AppMaterialModule
   ]
-})
+  })
 export class ProjectsModule { }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule
   ]
 })
 export class AppMaterialModule { }
+ /* Classe para rotas do pacote material */
