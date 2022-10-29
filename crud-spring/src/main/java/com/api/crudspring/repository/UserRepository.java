ea2b90c2117a07c2250081ce9db87432d717da25
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-10-29 11:47:30 -0300

    ...

--- crud-spring/src/main/java/com/api/crudspring/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.crudSpring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.crudSpring.model.UserModel;
-
-public interface UserRepository extends JpaRepository<UserModel, Integer> {
-}
