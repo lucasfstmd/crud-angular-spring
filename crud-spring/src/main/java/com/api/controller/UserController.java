58ba4714bf3ecead8a669d40601bff647d87b6f5
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-10-31 12:36:34 -0300

    added find by id

--- crud-spring/src/main/java/com/api/controller/UserController.java
+++ crud-spring/src/main/java/com/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.api.controller;
 import com.api.model.UserModel;
 import com.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +25,10 @@ public class UserController {
     @PostMapping("/save")
     public UserModel save(@RequestBody UserModel user){
         return repository.save(user);
+    }
+
+    @GetMapping(path = "/{Id}")
+    public ResponseEntity consult(@PathVariable("Id") Integer Id){
+        return repository.findById(Id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
     }
 }
