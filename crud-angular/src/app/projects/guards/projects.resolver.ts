9595d7720d194afde90f8d7fbd5da6cc868d03c9
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2022-11-01 10:59:23 -0300

    shared error

--- crud-angular/src/app/projects/guards/projects.resolver.ts
+++ crud-angular/src/app/projects/guards/projects.resolver.ts
@@ -1,21 +1,22 @@
-import { ProjectsModel } from './../models/projects.model';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 
 import { ProjectsService } from '../services/projects.service';
+import { ProjectsModel } from './../models/projects.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProjectsResolver implements Resolve<ProjectsModel> {
 
   constructor(private service: ProjectsService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProjectsModel> {
-    if(route.params && route.params['id']){
-      return this.service.loadById(route.params['id'])
+    if (route.params && route.params['id']) {
+      return this.service.loadById(route.params['id']);
     }
-    return of({_id: '', name: '', category: ''});
+    return of({ _id: '', name: '', category: '' });
   }
 }
